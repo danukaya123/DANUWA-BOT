90a98fdd6f7fcb9b9a45902033b7537cd5a25ddc
Danuka <danukaandhimasha@gmail.com>
2025-05-15 19:49:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Paste your session id here
+SESSION_ID: process.env.SESSION_ID || "vdQUSYTQ#-w4gJBdNwLAM6sEC1lt843pZPg-Zp5PgAMXEqjYTAlE", // Paste your session id here
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|51c173810f25c71beb51b431867187b736b89c28",
